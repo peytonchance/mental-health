492789ee6774429b34c13490004d384c363435a4
peytonchance <peytonchance@gmail.com>
2017-10-10 23:27:13 -0400

    Setup pagepiling and nav.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('#pagepiling').pagepiling({
+    anchors: ['page-zero', 'page-one', 'page-two', 'page-three', 'page-four'],
+    menu: '#myMenu',
+    keyboardScrolling: true,
+    sectionSelector: '.section',
+  });
+});
